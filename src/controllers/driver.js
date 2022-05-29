8e91fdce17c559cb5f993cef29d4a6ba977df2a0
Yekinni Ajibola <yekinnijibola@gmail.com>
2022-05-29 14:02:43 +0100

    feat: added get orders and order functionality

--- src/controllers/driver.js
+++ src/controllers/driver.js
@@ -1,9 +1,24 @@
 const db = require("../models/models/index");
 
 class Driver {
-  static async getOrders(req, res) {}
+  static async getOrders(req, res) {
+    const orders = await db.Order.findAll();
 
-  static async getOrder(req, res) {}
+    if (orders) {
+      return res.status(200).json({ data: orders, message: "fetched orders" });
+    }
+    res.status(500).send("failed to fetch orders");
+  }
+
+  static async getOrder(req, res) {
+    const id = req.body?.id;
+    if (!id) return res.status(400).send("id is empty");
+    const order = await db.Order.findOne({ where: { id } });
+    if (order) {
+      return res.status(200).json({ data: order, message: "fetched order" });
+    }
+    return res.status(500).send("failed to fetch order");
+  }
 
   static async decideOrder(req, res) {}
 }
